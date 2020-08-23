f26e8a897cacc0e53649c61013d6e069f1595191
dockutusu <dockutusu@gmail.com>
2020-08-23 15:55:22 -0400

    added zeynep and tarik

--- src/conflicts/StashMergeTrials.java
+++ src/conflicts/StashMergeTrials.java
@@ -3,6 +3,9 @@ package conflicts;
 public class StashMergeTrials {
 	public static void main(String[] args) {
 		System.out.println("Kerim Dogan");
+		System.out.println("Zeynep Dogan");
+		System.out.println("Tarik Dogan");
+
 
 
 	}
